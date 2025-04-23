7fbde6a1e3702c71b746bfa10545d7ee1fc6869d
CristinaFdezFdez <cristina.98fdz@gmail.com>
2025-04-23 09:59:16 +0200

    Hago que salgan las palabras en horizontal en la sopa

--- sopa_letras/cmd/sopa_letras.js
+++ sopa_letras/cmd/sopa_letras.js
@@ -1,6 +1,6 @@
-//import { createInterface } from 'readline';
+import { createInterface } from 'readline';
 // Palabras de la sopa de letras
-/*const palabras = ["raton", "sol", "nube", "tren", "vino", "luz", "casa", "gato", "lago", "flor"];*/
+const palabras = ["raton", "sol", "nube", "tren", "vino", "luz", "casa", "gato", "lago", "flor"];
 // Tamaño sopa de letras
 const SIZE = 20;
 
@@ -14,13 +14,22 @@ let palabrasFalladas = [];
 // Puntos del jugador
 let puntos = 0;
 // Configuramos readline para leer entrada del teclado
-/*const teclado = createInterface({
+const teclado = createInterface({
     input: process.stdin,
     output: process.stdout
-});*/
+});
 
 //Función para insertar palabras en la sopa horizontalmente
 function insertarPalabras() {
+    for (let palabra of palabras) {
+        let fila = Math.floor(Math.random() * SIZE);
+        let col = Math.floor(Math.random() * (SIZE - palabra.length));
+
+        // Insertamos la palabra
+        for (let i = 0; i < palabra.length; i++) {
+            sopa[fila][col + i] = palabra[i];
+        }
+    }
     for (let i = 0; i < SIZE; i++) {
         for (let j = 0; j < SIZE; j++) {
             if (!sopa[i][j]) {
@@ -29,8 +38,8 @@ function insertarPalabras() {
         }
     }
 }
-insertarPalabras();
 
+insertarPalabras();
 
 // Función para mostrar la sopa por consola 
 function imprimirConsola() {
