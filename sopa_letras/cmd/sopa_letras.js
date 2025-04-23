319609ef1633ed13228a303a9ab6192e0c8981ef
CristinaFdezFdez <cristina.98fdz@gmail.com>
2025-04-23 08:54:26 +0200

    Muestro una tabla 20*20 llena de letras del abecedario aleatorias

--- sopa_letras/cmd/sopa_letras.js
+++ sopa_letras/cmd/sopa_letras.js
@@ -1,13 +1,44 @@
-const readline = require('readline');
-//Palabras de la sopa de letras
-const palabras = ["raton", "sol", "nube", "tren", "vino", "luz", "casa", "gato", "lago", "flor"];
- //Tamaño sopa de letras
- const SIZE = 20;
+//import { createInterface } from 'readline';
+// Palabras de la sopa de letras
+/*const palabras = ["raton", "sol", "nube", "tren", "vino", "luz", "casa", "gato", "lago", "flor"];*/
+// Tamaño sopa de letras
+const SIZE = 20;
 
- //Array para sopa vacia y llenar de letras random
- let sopa = Array.from({length:SIZE}, ()=>Array(SIZE).fill(null));
- const letras = "abcdefghijklmnopqrstuvwxyz";
- 
- //Lista palabras no encontradas
- let palabrasFalladas=[];
+// Array para sopa vacia y llenar de letras random
+let sopa = Array.from({ length: SIZE }, () => Array(SIZE).fill(null));
+const letras = "abcdefghijklmnopqrstuvwxyz";
 
+// Lista palabras no encontradas
+let palabrasFalladas = [];
+
+// Puntos del jugador
+let puntos = 0;
+// Configuramos readline para leer entrada del teclado
+/*const teclado = createInterface({
+    input: process.stdin,
+    output: process.stdout
+});*/
+
+//Función para insertar palabras en la sopa horizontalmente
+function insertarPalabras() {
+    for (let i = 0; i < SIZE; i++) {
+        for (let j = 0; j < SIZE; j++) {
+            if (!sopa[i][j]) {
+                sopa[i][j] = letras[Math.floor(Math.random() * letras.length)];
+            }
+        }
+    }
+}
+insertarPalabras();
+
+
+// Función para mostrar la sopa por consola 
+function imprimirConsola() {
+    process.stdout.write("\x1Bc"); // Limpia la pantalla
+    process.stdout.write("\nSOPA DE LETRAS\n");
+    sopa.forEach(fila => process.stdout.write(fila.join(" ") + "\n"));
+    process.stdout.write("\nPalabras falladas: " + palabrasFalladas.join(", ") + "\n");
+    process.stdout.write(`Puntos ganados: ${puntos}\n`);
+}
+
+imprimirConsola();
